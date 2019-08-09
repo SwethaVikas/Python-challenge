2b9883c3ebaf436edff070cc9703174356959b7c
SwethaVikas <51687896+SwethaVikas@users.noreply.github.com>
2019-08-09 16:07:28 -0400

    PyBank_Complete

--- PyBank/main.py
+++ PyBank/main.py
@@ -7,12 +7,16 @@ budget_data_csv = os.path.join('Resources','budget_data.csv')
 # Initialize variables
 total_months = []
 total_Profit_Losses= []
-Monthly_Diff=[]
+Monthly_Diff = []
+Monthly_PL = []
 average_Profit_Losses = 0
-greatest_increase_profits_date = 0
-greatest_increase_profits_amount = 0
-greatest_decrease_losses_date = 0
-greatest_decrease_losses_amount = 0
+Max_increase_profits_date = 0
+Max_increase_profits_amount = 0
+Max_decrease_losses_date = 0
+Max_decrease_losses_amount = 0
+Inital_value = 0
+Final_value = 0
+Date = []
 
 # with open(budget_data_csv, newline="") as csvfile:
 with open(budget_data_csv, newline="") as csvfile:
@@ -32,7 +36,33 @@ with open(budget_data_csv, newline="") as csvfile:
         average_Profit_Losses = total_Profit_Losses/total_months
 
         # Greatest increase in profits date and Amount te over the entire period
-        Monthly_Diff = int(i[1])
+        Final_value = int(i(1))
+        Monthly_Diff = Final_value - Inital_value
+        Monthly_PL.append(Monthly_Diff)
+        Inital_value = Final_value
+        Max_increase_profits_amount = max(Monthly_PL)
+        Max_increase_profits_date = Date[Monthly_PL.index(Max_increase_profits_amount)]
+
+        # Greatest decrease in losses (date and amount) over the entire period
+        Max_decrease_losses_amount = min(Monthly_PL)
+        Max_decrease_losses_date = Date[Monthly_PL.index(Max_decrease_losses_amount)]
+
+        # Display Result
+        print("Financial Analysis") 
+        print("--------------------------------")
+        print("Total Months" + total_months)
+        print("Total" + total_Profit_Losses)
+        print("Average" +average_Profit_Losses )  
+        print("Greatest Increase in Profits" + Max_increase_profits_date + Max_increase_profits_amount )
+        print("Greatest Decrease in Profits" + Max_decrease_losses_date + Max_decrease_losses_amount) 
+
+
+
+
+
+
+        
+
 
 
 
